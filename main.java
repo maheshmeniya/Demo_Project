0941b974664875c52e0a3e56d0f2ac2080a1ef8e
vimal_moliya <vimalmoliya05@gmail.com>
2012-09-22 21:04:34 -0700

    2 commit

--- main.java
+++ main.java
@@ -2,6 +2,8 @@ public class main
 {
 	public static void main(String ... ar)
 	{
+		System.out.println("Git Introduction");
 		System.out.println("Git -- everything-is-local");
+		System.out.println("GitHub -- Social Coding");
 	}
 }
