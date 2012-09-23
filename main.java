1386047a05a945ae73780a56a8f789e1d024ea55
maheshmeniya <mjmeniya@gmail.com>
2012-09-22 23:51:46 -0700

    5 commit

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main
 {
 	public static void main(String ... ar)
 	{
+		System.out.println("Git");
 		System.out.println("Git Introduction");
 		System.out.println("Git -- everything-is-local");
 		System.out.println("By -");
